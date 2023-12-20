8759a26be82b04d627c518e176ce716c41815119
Mehmet Eren Akbulut <makbulut@student.42istanbul.com.tr>
2023-12-20 16:33:22 +0300

    [update]: Refactor WebSocket example and camera stream
    - Update index.html to display a camera stream using the received base64-encoded image.
    - Update websocket_server.py to include a function to send camera frames as base64-encoded images.
    - Clean up unnecessary parts of the previous WebSocket example in index.html.

--- websocket_server.py
+++ websocket_server.py
@@ -1,11 +1,35 @@
 import asyncio
 import websockets
+import cv2
+import base64
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+async def send_camera_frames(websocket, path):
+    camera = cv2.VideoCapture(0)
+    try:
+        while True:
+            ret, frame = camera.read()
 
-start_server = websockets.serve(echo, "0.0.0.0", 8000)
+            # Frame'i bir JPEG formatına çevir
+            _, buffer = cv2.imencode('.jpg', frame)
+
+            # Buffer'ı base64 formatına çevir
+            jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+
+            # Gelen veriyi istemciye gönder
+            await websocket.send(jpg_as_text)
+
+            # Kısa bir bekleme süresi ekleyebilirsiniz, gerekirse
+            # await asyncio.sleep(0.1)
+
+    finally:
+        # Kamera serbest bırakılıyor
+        camera.release()
+
+async def server_handler(websocket, path):
+    print(f"Yeni bağlantı: {path}")
+    await send_camera_frames(websocket, path)
+
+start_server = websockets.serve(server_handler, "0.0.0.0", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
