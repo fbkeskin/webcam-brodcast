d035b392c2189c6c7fe4f75a2db48cd7c1f03f92
Mehmet Eren Akbulut <makbulut@student.42istanbul.com.tr>
2023-12-21 13:00:01 +0300

    [update]: Configure SSL for WebSocket server, update nginx.conf, and adjust client-side code
    - Add SSL setup to the Dockerfile for the WebSocket server.
    - Update nginx.conf to use HTTPS for proxy_pass to the WebSocket server.
    - Adjust the client-side code in index.html to use the secure WebSocket protocol (wss://) and connect to the correct IP address.

--- websocket_server.py
+++ websocket_server.py
@@ -1,35 +1,19 @@
 import asyncio
+import pathlib
+import ssl
 import websockets
-import cv2
-import base64
 
-async def send_camera_frames(websocket, path):
-    camera = cv2.VideoCapture(0)
-    try:
-        while True:
-            ret, frame = camera.read()
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
 
-            # Frame'i bir JPEG formatına çevir
-            _, buffer = cv2.imencode('.jpg', frame)
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(
+    pathlib.Path('/makbulut.42.fr.crt'),
+    keyfile=pathlib.Path('/makbulut.42.fr.key'),
+)
 
-            # Buffer'ı base64 formatına çevir
-            jpg_as_text = base64.b64encode(buffer).decode('utf-8')
-
-            # Gelen veriyi istemciye gönder
-            await websocket.send(jpg_as_text)
-
-            # Kısa bir bekleme süresi ekleyebilirsiniz, gerekirse
-            # await asyncio.sleep(0.1)
-
-    finally:
-        # Kamera serbest bırakılıyor
-        camera.release()
-
-async def server_handler(websocket, path):
-    print(f"Yeni bağlantı: {path}")
-    await send_camera_frames(websocket, path)
-
-start_server = websockets.serve(server_handler, "0.0.0.0", 8000)
+start_server = websockets.serve(echo, "0.0.0.0", 8000, ssl=ssl_context)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
