5a3eeb49f09f43f77d0bdcac6788e2278077b90a
Mehmet Eren Akbulut <makbulut@student.42istanbul.com.tr>
2023-12-24 11:23:22 +0300

    [update]: Update Makefile, docker-compose.yml, index.html, nginx.conf, and websocket_server.py
    - Update Makefile to check if containers are already running before attempting to start them.
    - Modify docker-compose.yml to include restart: always for services.
    - Update index.html to use the correct WebSocket address (ws://10.19.19.40:8000).
    - Modify nginx.conf to proxy_pass to the correct WebSocket address (http://10.19.19.40:8000).
    - Adjust websocket_server.py to send camera frames and handle connections properly.

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
+
 DOCKER=sudo docker
+CONTAINER_COUNT := $(shell $(DOCKER)-compose ps -q | wc -l)
 
 all:
+ifeq ($(CONTAINER_COUNT), 0)
 	$(DOCKER)-compose up --build -d
+else
+	@echo "Containers are already running."
+endif
+	nohup /opt/homebrew/bin/python3 websocket_server.py &
 
 rm:
 	$(DOCKER)-compose down --rmi all --volumes
+	pkill -f websocket_server.py
 
 re-all: rm all
 
--- websocket_server.py
+++ websocket_server.py
@@ -1,20 +1,35 @@
 import asyncio
-import pathlib
-import ssl
 import websockets
+import cv2
+import base64
 
-async def echo(websocket, path):
-    async for message in websocket:
-        await websocket.send(message)
+async def send_camera_frames(websocket, path):
+    camera = cv2.VideoCapture(0)
+    try:
+        while True:
+            ret, frame = camera.read()
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(
-    pathlib.Path('/makbulut.42.fr.crt'),
-    keyfile=pathlib.Path('/makbulut.42.fr.key'),
-)
+            # Frame'i bir JPEG formatına çevir
+            _, buffer = cv2.imencode('.jpg', frame)
 
-start_server = websockets.serve(echo, "0.0.0.0", 8000, ssl=ssl_context)
+            # Buffer'ı base64 formatına çevir
+            jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+
+            # Gelen veriyi istemciye gönder
+            await websocket.send(jpg_as_text)
+
+            # Kısa bir bekleme süresi ekleyebilirsiniz, gerekirse
+            await asyncio.sleep(0.1)
+
+    finally:
+        # Kamera serbest bırakılıyor
+        camera.release()
+
+async def server_handler(websocket, path):
+    print(f"Yeni bağlantı: {path}")
+    await send_camera_frames(websocket, path)
+
+start_server = websockets.serve(server_handler, "0.0.0.0", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
